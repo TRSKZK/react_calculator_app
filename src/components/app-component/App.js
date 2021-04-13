762a0ee5e2e7a5758b7244811d52aca600421f7e
TRSKZK <75508554+TRSKZK@users.noreply.github.com>
2021-04-13 14:07:54 +0300

    styled components added

--- src/components/app-component/App.js
+++ src/components/app-component/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
 
 function App() {
-    return (
-    <div className="calc-container"><h1>hello REct world</h1></div>
-)
+  return (
+    <AppWrapper>
+      <div className="calc-container">
+        <h1>hello REct world</h1>
+      </div>
+    </AppWrapper>
+  );
 }
 
 export default App;
