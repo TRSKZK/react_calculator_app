9d24c0e05d54ed7c89504c7b4c94e761525df145
TRSKZK <75508554+TRSKZK@users.noreply.github.com>
2021-04-15 12:41:52 +0300

    Replace

--- src/components/numbers/numbers.js
+++ src/components/numbers/numbers.js
@@ -70,7 +70,7 @@ const OperationBtns = styled(ButtonNumber)`
 function RenderNumbers() {
   const [data, setData] = useState("");
   const calcBtns = [];
-  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, `,`, `%`].forEach((btn) => {
+  [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, `.`, `%`].forEach((btn) => {
     calcBtns.push(
     <ButtonNumber
       onClick={(e) => {
